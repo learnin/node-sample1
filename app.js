2f4524779eaf6b49e8719b4118cd5e98bfec9204
learnin <i.learnin@gmail.com>
2012-07-01 19:43:26 +0900

    change from jade to ejs. add routing

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
 
 var app = module.exports = express.createServer();
 
@@ -12,7 +11,7 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -28,8 +27,47 @@ app.configure('production', function(){
 });
 
 // Routes
+var fs = require('fs'),
+  path = require('path');
 
-app.get('/', routes.index);
+var setupRouting = function(routesDir) {
+  var files = fs.readdirSync(routesDir);
+  files.forEach(function(e) {
+    e = routesDir + '/' + e;
+    var stats = fs.statSync(e);
+    if (stats.isDirectory()) {
+      setupRouting(e);
+    } else {
+      var urlPath = '/';
+      if (path.basename(e, '.js') == 'index') {
+        urlPath += path.dirname(path.relative(routesDir, e));
+      } else {
+        urlPath += path.relative(routesDir, e).replace('.js', '');
+      }
+      urlPath = path.normalize(urlPath);
+      var handler = require(e);
+      for (var f in handler) {
+        switch(f) {
+          case 'index':
+            app.get(urlPath, handler[f]);
+            break;
+          case 'create':
+            app.post(urlPath, handler[f]);
+            break;
+          case 'update':
+            app.put(urlPath + '/:id', handler[f]);
+            break;
+          case 'destroy':
+            app.del(urlPath + '/:id', handler[f]);
+            break;
+          default:
+            app.get(urlPath + '/' + f, handler[f]);
+        }
+      }
+    }
+  });
+}
+setupRouting(path.resolve(__dirname + '/routes'));
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
